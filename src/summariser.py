9df9e1b22b8a9ae6e618a025ace1f570a5d2e8f7
DevG06 <devarshi.dg56@gmail.com>
2024-09-03 03:12:20 +0530

    Summariser

--- src/summariser.py
+++ src/summariser.py
@@ -1,43 +1,38 @@
 import pandas as pd
-import os
 import re
 import google.generativeai as genai
+from dotenv import load_dotenv
+import os
 
-# Load your dataset
-df = pd.read_csv('Dataset/Single_Hotel_Dataset.csv')
+class Summariser:
+    def __init__(self):
+        load_dotenv()
+        self.google_api_key = os.getenv('GOOGLE_API_KEY')
+    
+    def summarize_reviews(self, data, aspects=None):
 
-# Concatenate all reviews into one string
-all_reviews = " ".join(df['REVIEWS'].tolist())
+        # Concatenate all reviews into one string
+        all_reviews = " ".join(data['REVIEWS'].tolist())
 
-# Retrieve the API key from Colab’s secrets
-GOOGLE_API_KEY = 'AIzaSyCdU-qCf2UmeF8lmvcH2AYfNgdpQzYgBs8'
+        # Configure the API key for Google Generative AI
+        genai.configure(api_key=self.google_api_key)
 
-# Configure the API key for Google Generative AI
-genai.configure(api_key=GOOGLE_API_KEY)
+        # Initialize the GenerativeModel with the Gemini summarization model
+        model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
-# Gemini 1.5 Flash
+        aspects = []
 
-# Initialize the GenerativeModel with the Gemini summarization model
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+        # Create the prompt based on whether aspects are provided
+        if aspects:
+            prompt = f"Provide a summary of the hotel reviews, focusing just on the following aspects: {', '.join(aspects)} in short."
+        else:
+            prompt = "Provide an overall pointwise summary of the hotel reviews, focusing on key strengths and weaknesses in short"
 
-# Define the list of aspects you want to focus on
+        # Generate the summary for all reviews
+        response = model.generate_content([prompt, all_reviews])
 
-#aspects = ["Location", "Parking", "Cleanliness", "Transportation", "Internet", "Restaurant"]
+        # Remove `**` (bold formatting) and `##` (heading formatting) from the summary
+        cleaned_text = re.sub(r'\*\*', '', response.text)
+        cleaned_output = re.sub(r'## ', '', cleaned_text)
 
-aspects = []
-
-# Create the prompt based on whether aspects are provided
-if aspects:
-    prompt = f"Provide a summary of the hotel reviews, focusing just on the following aspects: {', '.join(aspects)} in short."
-else:
-    prompt = "Provide an overall pointwise summary of the hotel reviews, focusing on key strengths and weaknesses in short"
-
-# Generate the summary for all reviews
-response = model.generate_content([prompt, all_reviews])
-
-# Remove `**` (bold formatting) and `##` (heading formatting) from the summary
-cleaned_text = re.sub(r'\*\*', '', response.text)
-cleaned_output = re.sub(r'## ', '', cleaned_text)
-
-# Print the summarized text
-print(cleaned_output)
+        return cleaned_output
